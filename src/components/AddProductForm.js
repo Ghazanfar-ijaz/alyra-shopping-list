b4776309dae941b57a45e9c85496b60305114e6a
Ghazanfar-ijaz <ghazanfar@outlook.fr>
2021-04-06 17:42:23 +0200

    push

--- src/components/AddProductForm.js
+++ src/components/AddProductForm.js
@@ -1,29 +1,28 @@
-import ShoppingApp from "./ShoppingApp"
-
 const AddProductForm = (props) => {
-  const { addShoppingList } = props
+  const { shopping, addShoppingList } = props
+
   const handleFormSubmit = (event) => {
     event.preventDefault()
     console.log(event.elements.product.value)
     const newProduct = event.target.elements.product.value
-    if (!Shopping.includes)
-      addToShoppingList
-  }
-  return (
-    <form className="mb-5" onSubmit={handleFormSubmit}>
-      <div className="input-group mb-2">
-        <input
-          id="product"
-          className="form-control"
-          aria-label="Ajouter sur la liste"
-          required
-        />
-        <button type="submit" className="btn btn-success btn-lg">
-          J'ajoute !
+    if (!Shopping.includes(newProduct)) {
+      addToShoppingList(newProduct)
+    }
+    return (
+      <form className="mb-5" onSubmit={handleFormSubmit}>
+        <div className="input-group mb-2">
+          <input
+            id="product"
+            className="form-control"
+            aria-label="Ajouter sur la liste"
+            required
+          />
+          <button type="submit" className="btn btn-success btn-lg">
+            J'ajoute !
         </button>
-      </div>
-    </form>
-  )
-}
+        </div>
+      </form>
+    )
+  }
 
-export default AddProductForm
+  export default AddProductForm
